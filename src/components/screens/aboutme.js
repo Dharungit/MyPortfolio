27b51421be3ca35a710d802953ea6f0b66231a57
Dharun <dharun123wizz@gmail.com>
2021-11-27 20:26:20 +0530

    error patching

--- src/components/screens/aboutme.js
+++ src/components/screens/aboutme.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from "react";
-import profile from "../../assets/profile-1.png";
+import profile from "../../assets/dharun-modified.png";
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 
@@ -9,7 +9,7 @@ function Aboutme() {
   }, []);
 
   return (
-    <div className="mx-5 font-pop">
+    <div id="about"className="mx-5 font-pop">
       <div className="about-continer">
         <h1 data-aos="fade-down" className="text-4xl font-semibold mb-10 about-title ">About me.</h1>
         <div className="circle-continer flex justify-center ">
@@ -21,13 +21,13 @@ function Aboutme() {
             className="w-32 mb-5 profile-img rounded-full border-2 border-solid"
           />
         </div>
-        <p data-aos="fade-up" data-aos-anchor-placement="top-bottom" className="text-center  font-semibold">
-          Hi There! I'm <span className="text-red-600">Dharun</span>  R, I am a Software Engineer and a python
+        <p data-aos="fade-up" data-aos-anchor-placement="top-bottom" className="text-center  font-light">
+          Hi There! I'm <span className="text-red-600">Dharun</span>  R, I'm a Software Engineer and a python
           enthusiastic developer with a good background in web development,
           machine learning and problem-solving mindset to solve the real-world
           problems.
         </p>
-        <p data-aos="fade-up" data-aos-anchor-placement="top-bottom" className="mt-3 mb-20 h-32 text-center font-semibold">
+        <p data-aos="fade-up" data-aos-anchor-placement="top-bottom" className="mt-3 mb-20 h-32 text-center font-light">
           Currently, an <span className="text-red-600"> Undergraduate </span> majoring in Computer Science and
           Engineering at KPRIET, Coimbatore.
         </p>
--- src/components/screens/hero.js
+++ src/components/screens/hero.js
@@ -11,7 +11,7 @@ function hero() {
             </div>
             <div className="social-links flex justify-center space-x-6 mt-5 tracking-in-expand-2">
                 <a href="http://www.linkedin.com/in/dharun-r"><img src="https://img.icons8.com/ios-filled/500/000000/linkedin.png" width="30" alt="img"/></a>
-                <a href="https://github.com/Dharungit"><img src="https://img.icons8.com/ios-glyphs/480/000000/github.png" width="30"/></a>
+                <a href="https://github.com/Dharungit"><img src="https://img.icons8.com/ios-glyphs/480/000000/github.png" width="30" alt="img"/></a>
                 <a href="www.google.com"><img src="https://img.icons8.com/ios-glyphs/480/000000/instagram-new.png" width="30" alt="img"/></a>
             </div>
             </div>  
